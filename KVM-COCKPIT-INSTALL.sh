2d952803a60f1b10181a66b15f0dda0370c19f6c
Brian Weenk <60255415+BrianWeenkGames@users.noreply.github.com>
2024-03-20 00:31:38 +0100

    Update KVM-COCKPIT-INSTALL.sh

--- KVM-COCKPIT-INSTALL.sh
+++ KVM-COCKPIT-INSTALL.sh
@@ -7,11 +7,14 @@ dnf -y install @virt
 lsmod | grep kvm 
 #Install KVM Management tools#
 dnf -y install virt-top libvirt-devel libguestfs-tools
+
 #Start and enable KVM daemon#
 systemctl start libvirtd
 systemctl enable libvirtd
+
 #Check if service is running !!DO NOT USE IN SCRIPT!!#
 #systemctl status libvirtd
+
 #Install Virtual machine manager#
 dnf -y install virt-manager
 
@@ -19,6 +22,7 @@ dnf -y install virt-manager
 yum -y install cockpit
 systemctl enable --now cockpit.socket
 dnf install cockpit-machines.noarch
+
 #Open the firewall#
 firewall-cmd --permanent --zone=public --add-service=cockpit
 firewall-cmd --reload
