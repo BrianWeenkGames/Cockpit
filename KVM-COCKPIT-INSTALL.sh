9abeaf16669243d055436abd9b7ad74c83b5a8fc
Brian Weenk <60255415+BrianWeenkGames@users.noreply.github.com>
2025-07-25 10:35:06 +0200

    Update KVM-COCKPIT-INSTALL.sh
    Undid changes as its not stable

--- KVM-COCKPIT-INSTALL.sh
+++ KVM-COCKPIT-INSTALL.sh
@@ -1,12 +1,28 @@
 #-----------------------------------------------------------UPDATE-DNF-----------------------------------------------------------#
 dnf -y update
 
+#-----------------------------------------------------------INSTALL-KVM-----------------------------------------------------------#
+dnf -y install @virt
+#Check if Kernel modules are loaded#
+lsmod | grep kvm 
+#Install KVM Management tools#
+dnf -y install virt-top libvirt-devel libguestfs-tools
+
+#Start and enable KVM daemon#
+systemctl start libvirtd
+systemctl enable libvirtd
+
+#Check if service is running !!DO NOT USE IN SCRIPT!!#
+#systemctl status libvirtd
+
+#Install Virtual machine manager#
+dnf -y install virt-manager
+
 #-----------------------------------------------------------INSTALL-COCKPIT-----------------------------------------------------------#
-dnf install -y cockpit
-
-#Enable the service to start on boot:#
+yum -y install cockpit
 systemctl enable --now cockpit.socket
+dnf install cockpit-machines.noarch
 
-#Allow the service through the firewall:#
+#Open the firewall#
 firewall-cmd --permanent --zone=public --add-service=cockpit
 firewall-cmd --reload
